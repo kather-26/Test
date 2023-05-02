55cd2e542b370e4ba74b35dd78f80c474b0e5365
unknown <mail2katheroli@gmail.com>
2023-05-02 12:59:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const { Pool } = require('pg');
+const express = require('express');
+
+const app = express();
+
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'networkcard',
+  host: 'localhost',
+  database: 'Test',
+  port: 5432 // default PostgreSQL port
+});
+
+app.get('/api/users/:username/password/:password', async (req, res) => {
+    const { username, password } = req.params;
+    try {
+      const query = `
+        SELECT username, password 
+        FROM users 
+        WHERE username = $1 AND password = $2;
+      `;
+      const result = await pool.query(query, [username, password]);
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      const { username: foundUsername, password: foundPassword } = result.rows[0];
+      return res.json({ username: foundUsername, password: foundPassword });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  
+
+  
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
